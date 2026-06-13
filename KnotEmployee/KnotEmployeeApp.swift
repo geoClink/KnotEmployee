6fb99a33b34038224e59cec2b5bf1590cdbba2bb
George Clinkscales <1lclink2@att.net>
2026-06-13 12:13:56 -0400

    Initial Commit

---
+++ KnotEmployee/KnotEmployeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnotEmployeeApp.swift
+//  KnotEmployee
+//
+//  Created by George Clinkscales on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct KnotEmployeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
