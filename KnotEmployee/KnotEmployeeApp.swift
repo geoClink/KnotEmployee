6cc2c6f486ac7a214b6d8954c1e0d37ec63edda0
George Clinkscales <1lclink2@att.net>
2026-06-14 02:20:24 -0400

    Clock banner + Home restructure into status/today/upcoming

--- KnotEmployee/KnotEmployeeApp.swift
+++ KnotEmployee/KnotEmployeeApp.swift
@@ -10,6 +10,22 @@ import SwiftUI
 @main
 struct KnotEmployeeApp: App {
     @State private var store = AppStore.sample
+
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        let ink = UIColor(red: 0x2A/255, green: 0x18/255, blue: 0x10/255, alpha: 1)
+        if let large = UIFont(name: "CormorantGaramond-Medium", size: 36) {
+            appearance.largeTitleTextAttributes = [.font: large, .foregroundColor: ink]
+        }
+        if let inline = UIFont(name: "CormorantGaramond-Medium", size: 20) {
+            appearance.titleTextAttributes = [.font: inline, .foregroundColor: ink]
+        }
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+    }
+
     var body: some Scene {
         WindowGroup {
             RootView()
